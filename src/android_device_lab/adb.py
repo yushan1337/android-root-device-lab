869aa6b7e17abfcb6fa18c679e1143194516a3e4
yushan1337 <koujian1337@gmail.com>
2026-07-12 11:15:13 +0800

    新增电池信息获取功能，更新 adb.py，添加电池信息字段及获取逻辑

--- src/android_device_lab/adb.py
+++ src/android_device_lab/adb.py
@@ -12,6 +12,12 @@ class DeviceInfo:
     build_fingerprint: str
     brand: str
     security_patch: str
+@dataclass
+class BetteryInfo:
+    temperature: str = "N/A"  # 默认值为 "N/A"，表示未获取到温度信息
+    ac_power_status: str = "N/A"  # 默认值为 "N/A"，表示未获取到交流电状态信息
+    voltage: str = "N/A"  # 默认值为 "N/A"，表示未获取到电压信息
+    level: str = "N/A"  # 默认值为 "N/A"，表示未获取到电量信息  
 from android_device_lab.command import CommandResult, run_command
 def list_devices() -> CommandResult:
     result = run_command(["adb", "devices"])
@@ -27,7 +33,18 @@ def get_device_info() -> DeviceInfo:
         sdk_version=run_command(["adb", "shell", "getprop", "ro.build.version.sdk"]).stdout.strip(),
         build_fingerprint=run_command(["adb", "shell", "getprop", "ro.build.fingerprint"]).stdout.strip(),
         brand=run_command(["adb", "shell", "getprop", "ro.product.brand"]).stdout.strip(),
-        security_patch=run_command(["adb", "shell", "getprop", "ro.build.version.security_patch"]).stdout.strip()
+        security_patch=run_command(["adb", "shell", "getprop", "ro.build.version.security_patch"]).stdout.strip(),
     )
     logging.info(f"获取设备信息完成: {device_info}")
     return device_info
+def get_battery_info() -> BetteryInfo:
+    logging.info("开始获取电池信息...")
+    line = run_command(["adb", "shell", "dumpsys", "battery"]).stdout.strip()
+    print(repr(line))
+    parts = line.split(":")
+    print(parts)
+    battery_info = BetteryInfo(
+       
+    )
+    logging.info(f"获取电池信息完成: {battery_info}")
+    return battery_info
